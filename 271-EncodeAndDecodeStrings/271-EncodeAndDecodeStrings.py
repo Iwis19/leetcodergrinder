b7d1b33f838ad12f6687f8469f882853f247f7df
Ronnie <167436841+Iwis19@users.noreply.github.com>
2026-03-15 22:45:24 -0400

    Create 271-EncodeAndDecodeStrings

---
+++ 271-EncodeAndDecodeStrings/271-EncodeAndDecodeStrings.py
@@ -0,0 +1,34 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+
+        """
+        did not solve on first submission, tried using delimiter but turned out i had to also include the length of the word.
+
+        NOTE: it's also easier if i have delimiter after the length of subsequent word as i can just start reading after the delimiter
+        """
+
+        encoded = ""
+        for i, string in enumerate(strs):
+            encoded+=f"{len(string)}/{string}"
+        return encoded
+
+    def decode(self, s: str) -> List[str]:
+        decoded = []
+        i = 0
+
+        """
+        if char is a delimiter(/), then i read the number (x) after it, and i take x subsequent characters, and add it
+        to temp word, then to array, then go next word
+        """
+        temp_length = 0
+        temp_word = ""
+        while i < len(s):
+            j = i
+            while s[j] != "/":
+                j+=1
+            length = int(s[i:j])
+            decoded.append(s[j+1:j+1+length])
+            i = j+1+length
+        return decoded
+

60ec46fc327888e9df292e389cfe1565074baa66
Ronnie <167436841+Iwis19@users.noreply.github.com>
2026-03-25 13:14:15 -0400

    File updated at 3/25/2026, 1:14:15 PM

--- 12-IntegertoRoman/12-IntegertoRoman.py
+++ 12-IntegertoRoman/12-IntegertoRoman.py
@@ -1,20 +1,27 @@
-# Last updated: 3/25/2026, 1:13:49 PM
+# Last updated: 3/25/2026, 1:14:15 PM
 1class Solution:
 2    def maxArea(self, height: List[int]) -> int:
 3
-4        left = 0
-5        right = len(height)-1
-6        max_area = 0
+4        """
+5        LEARNED from problem:
+6        1. 
 7
-8        while left < right:
-9
-10            area = min(height[left], height[right]) * (right - left)
-11            max_area = max(max_area, area)
-12
-13            if height[left] >= height[right]: right -= 1
-14            elif height[right] > height[left]: left += 1
-15
-16        return max_area
-17
-18
-19        
+8        67 ms 
+9        """
+10
+11        left = 0
+12        right = len(height)-1
+13        max_area = 0
+14
+15        while left < right:
+16
+17            area = min(height[left], height[right]) * (right - left)
+18            max_area = max(max_area, area)
+19
+20            if height[left] >= height[right]: right -= 1
+21            elif height[right] > height[left]: left += 1
+22
+23        return max_area
+24
+25
+26        

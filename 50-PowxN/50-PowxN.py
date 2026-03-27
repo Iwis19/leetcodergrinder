41b5c0ded961cde691f0f0df8e87acfad5b0ea90
Ronnie <167436841+Iwis19@users.noreply.github.com>
2026-03-27 18:04:58 -0400

    Create 50-PowxN

---
+++ 50-PowxN/50-PowxN.py
@@ -0,0 +1,11 @@
+# Last updated: 3/27/2026, 6:04:57 PM
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        
+        """
+        this is the stupid monkey solution i dont like it
+
+        0 ms beats 100%
+        """
+
+        return x**n

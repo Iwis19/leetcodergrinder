60151050cd57f0216338f14551659796d39d2c09
Ronnie <167436841+Iwis19@users.noreply.github.com>
2026-03-27 00:43:05 -0400

    Create 2231-FindFirstPalindromicStringInTheArray

---
+++ 2231-FindFirstPalindromicStringInTheArray/2231-FindFirstPalindromicStringInTheArray.py
@@ -0,0 +1,23 @@
+# Last updated: 3/27/2026, 12:43:04 AM
+class Solution:
+    def firstPalindrome(self, words: List[str]) -> str:
+
+        """
+        1. good string splice use i remember this now string == string[::-1]
+        2. i liked the in sol function creation
+
+        0ms runtime 100%
+        """
+
+        def isPalindromic(string:str) -> bool:
+
+            return True if string == string[::-1] else False
+
+        for word in words:
+
+            if isPalindromic(word):
+                return word
+        
+        return ""
+
+        

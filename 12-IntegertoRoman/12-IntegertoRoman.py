b084bedc19625ed8bccc27806b6460189c1eaa33
Ronnie <167436841+Iwis19@users.noreply.github.com>
2026-03-27 13:44:25 -0400

    File updated at 3/27/2026, 1:44:32 PM

--- 12-IntegertoRoman/12-IntegertoRoman.py
+++ 12-IntegertoRoman/12-IntegertoRoman.py
@@ -1,36 +1,18 @@
-# Last updated: 3/25/2026, 1:36:38 PM
+# Last updated: 3/27/2026, 1:44:32 PM
 1class Solution:
-2    def maxArea(self, height: List[int]) -> int:
-3
-4        """
-5        1. didnt have a clue on how to solve this, thought about two pointer but i thought it wouldnt work since i thought 
-6        it would miss out on the future best cases
-7        2. i originally had a solution where it kept track of max height column, and calculated area with it with the 
-8        current column i was on from a few weeks back, so it was good i thought about two pointer at least
-9
-10        LOGIC LEARNED:
-11        - either certainly decrease the area by moving the taller column closer to center, as the width already decreases
-12        - or maybe increase the area by moving the shorter column closer to center, altho width decreases
+2    def twoSum(self, nums: List[int], target: int) -> List[int]:
+3        
+4        map = {}
+5
+6        for i,num in enumerate(nums):
+7            map[num] = i
+8
+9        for index, current in enumerate(nums):
+10
+11            complement = target - current
+12            if complement in map and index != map[complement]:
 13
-14        this is a GREEDY solution, and it works here because the locally optimal outcome doesnt mess up the optimal
-15        future outcome like i said in pt 1
-16
-17        55 ms runtime beats 73% | 29 mb memory beats 80%
-18        """
-19
-20        left = 0
-21        right = len(height)-1
-22        max_area = 0
-23
-24        while left < right:
-25
-26            area = min(height[left], height[right]) * (right - left)
-27            max_area = max(max_area, area)
-28
-29            if height[left] >= height[right]: right -= 1
-30            elif height[right] > height[left]: left += 1
-31
-32        return max_area
-33
-34
-35        
+14                return [index, map[complement]]
+15
+16        return []
+17

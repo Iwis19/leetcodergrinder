9c0aa0addfc90556e121b02af71d002d1f80eb5e
Ronnie <167436841+Iwis19@users.noreply.github.com>
2026-03-28 17:09:40 -0400

    Create 167-TwoSumII (two pointer).py

---
+++ 167-TwoSumII/167-TwoSumII (two pointer).py
@@ -0,0 +1,22 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+
+        """
+        yay 2 ptr
+
+        0 ms runtime 100%
+        """
+        
+        left = 0
+        right = len(numbers)-1
+
+        while left < right:
+
+            sum = numbers[left] + numbers[right]
+            if sum == target: return [left+1, right+1]
+            elif sum > target: right -= 1
+            elif sum < target: left += 1
+
+        return [None, None]
+
+

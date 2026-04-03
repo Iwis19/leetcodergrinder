e9333fa348cad27d95eff2af779137d91c71e1f6
Ronnie <167436841+Iwis19@users.noreply.github.com>
2026-04-03 11:10:13 -0400

    Create 3024-TypeofTriangle

---
+++ 3024-TypeofTriangle/3024-TypeofTriangle.py
@@ -0,0 +1,13 @@
+# Last updated: 4/3/2026, 11:10:13 AM
+1class Solution:
+2    def triangleType(self, nums: List[int]) -> str:
+3        
+4        if nums[0] >= nums[1] + nums[2] or nums[1] >= nums[0] + nums[2] or nums[2] >= nums[0] + nums[1]:
+5            return "none"
+6
+7        if nums[0] == nums[1] == nums[2]:
+8            return "equilateral"
+9        if nums[0] == nums[1] or nums[1] == nums[2] or nums[2] == nums[0]:
+10            return "isosceles"
+11        else:
+12            return "scalene"

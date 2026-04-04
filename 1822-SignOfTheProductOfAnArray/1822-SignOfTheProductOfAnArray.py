60ad4bc3eff0c4508a6b6dfa8fe010f08e1a0418
Ronnie <167436841+Iwis19@users.noreply.github.com>
2026-04-03 21:55:39 -0400

    Create 1822-SignOfTheProductOfAnArray.py

---
+++ 1822-SignOfTheProductOfAnArray/1822-SignOfTheProductOfAnArray.py
@@ -0,0 +1,19 @@
+class Solution:
+    def arraySign(self, nums: List[int]) -> int:
+
+        def signFunc(x:int) -> int:
+
+            if x>0:
+                return 1
+            if x<0:
+                return -1
+            if x == 0:
+                return 0
+
+        product = 1
+
+        for num in nums:
+            product*=num
+
+        return signFunc(product)
+        

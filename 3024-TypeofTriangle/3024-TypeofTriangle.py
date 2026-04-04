9104c29e7d340638e03c8f9770b69d2a2a7ca1e2
Ronnie <167436841+Iwis19@users.noreply.github.com>
2026-04-03 21:38:36 -0400

    Update 3024-TypeofTriangle.py

--- 3024-TypeofTriangle/3024-TypeofTriangle.py
+++ 3024-TypeofTriangle/3024-TypeofTriangle.py
@@ -1,12 +1,12 @@
-# Last updated: 4/3/2026, 9:37:00 PM
-1class Solution:
-2    def findWordsContaining(self, words: List[str], x: str) -> List[int]:
-3        
-4        res = []
-5
-6        for i, word in enumerate(words):
-7
-8            if x in word:
-9                res.append(i)
-10
-11        return res
+class Solution:
+    def triangleType(self, nums: List[int]) -> str:
+        
+        if nums[0] >= nums[1] + nums[2] or nums[1] >= nums[0] + nums[2] or nums[2] >= nums[0] + nums[1]:
+            return "none"
+
+        if nums[0] == nums[1] == nums[2]:
+            return "equilateral"
+        if nums[0] == nums[1] or nums[1] == nums[2] or nums[2] == nums[0]:
+            return "isosceles"
+        else:
+            return "scalene"

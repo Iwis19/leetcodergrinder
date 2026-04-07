22958e43cb0c50edf4f79cb8c0ad9e945fe89984
Ronnie <167436841+Iwis19@users.noreply.github.com>
2026-04-06 20:56:20 -0400

    Update 2069-WalkingRobotSimulationII.py

--- 2069-WalkingRobotSimulationII/2069-WalkingRobotSimulationII.py
+++ 2069-WalkingRobotSimulationII/2069-WalkingRobotSimulationII.py
@@ -1,47 +1,59 @@
-# Last updated: 4/6/2026, 8:49:28 PM
-1class Robot:
-2
-3    def __init__(self, width: int, height: int):
-4        self.dir = 1
-5        self.pos = [0,0]
-6        self.w = width
-7        self.h = height
-8        self.move = [[0,1], [1,0], [0,-1], [-1,0]]
-9
-10    def step(self, num: int) -> None:
-11        x,y = self.getPos()
-12        cycle = 2 * (self.w + self.h) - 4
-13        num %= cycle
-14
-15        if num == 0:
-16            num = cycle
-17
-18        while num > 0:
-19            nx = x + self.move[self.dir][0]
-20            ny = y + self.move[self.dir][1]
-21
-22            if nx < 0 or nx >= self.w or ny < 0 or ny >= self.h:
-23                self.dir = (self.dir - 1) % 4
-24                continue
-25
-26            x,y = nx, ny
-27            num -= 1
-28        
-29        self.pos = [x,y]
-30
-31
-32    def getPos(self) -> List[int]:
-33        return self.pos
-34
-35    def getDir(self) -> str:
-36        if self.dir == 0: return "North"
-37        if self.dir == 1: return "East"
-38        if self.dir == 2: return "South"
-39        if self.dir == 3: return "West"
-40
-41
-42# Your Robot object will be instantiated and called as such:
-43# obj = Robot(width, height)
-44# obj.step(num)
-45# param_2 = obj.getPos()
-46# param_3 = obj.getDir()
+class Robot:
+
+
+    """
+    didnt really realize that the robot only moves along the perimeter, idk how i missed that (since it only turns when it meets bounds, it must be walking @ perimeter since it starts in a corner)
+
+    calculat perimeter cuz (width-1) + (height-1) + (width-1) + (height-1) 
+    """
+
+
+    def __init__(self, width: int, height: int):
+        self.dir = 1
+        self.pos = [0,0]
+        self.w = width
+        self.h = height
+        self.move = [[0,1], [1,0], [0,-1], [-1,0]]
+        self.perimeter = cycle = 2 * (self.w + self.h) - 4
+
+
+    def step(self, num: int) -> None:
+        x,y = self.getPos()
+       
+        num %= self.perimeter
+        if num == 0:
+            num = self.perimeter
+
+        while num > 0:
+            nx = x + self.move[self.dir][0]
+            ny = y + self.move[self.dir][1]
+
+            if nx < 0 or nx >= self.w or ny < 0 or ny >= self.h:
+                self.dir = (self.dir - 1) % 4
+                continue
+
+            x,y = nx, ny
+            num -= 1
+       
+        self.pos = [x,y]
+
+
+    def getPos(self) -> List[int]:
+        return self.pos
+
+
+    def getDir(self) -> str:
+        if self.dir == 0: return "North"
+        if self.dir == 1: return "East"
+        if self.dir == 2: return "South"
+        if self.dir == 3: return "West"
+
+
+
+
+# Your Robot object will be instantiated and called as such:
+# obj = Robot(width, height)
+# obj.step(num)
+# param_2 = obj.getPos()
+# param_3 = obj.getDir()
+

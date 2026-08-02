0f2613b684371b08561e0960391b9bdc702e484f
Ronnie <167436841+iwis19@users.noreply.github.com>
2026-08-01 22:00:19 -0400

    Update 877-StoneGame.py

--- 877-StoneGame/877-StoneGame.py
+++ 877-StoneGame/877-StoneGame.py
@@ -1,3 +1,10 @@
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
+
+        """
+        what the cheese
+
+        0 ms runtime beats 100% O(1)
+        """
+        
         return True
